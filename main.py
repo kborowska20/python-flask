6f3dc0cc5b8f443520c7a869f9feb8da6680f9de
klaudia borowska <canimi18@gmial.com>
2017-03-30 10:19:52 +0200

    todo app route:/,/add, edit and Bootstrap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from models.todo import Todo
 
 
@@ -9,22 +9,34 @@ app = Flask(__name__)
 def list():
     """ Shows list of todo items stored in the database.
     """
-    return "Hello World!"
+    todolist = Todo.get_all()
+    return render_template('index.html', todolist=todolist)
 
-
-@app.route("/add", methods=['GET', 'POST'])
+@app.route("/add", methods=['GET','POST'])
 def add():
     """ Creates new todo item
     If the method was GET it should show new item form.
     If the method was POST it shold create and save new todo item.
     """
-    return "Add todo"
+    if request.method == 'GET':
+        return render_template('add.html')
+
+    elif request.method == 'POST':
+
+        name = request.form['name']
+        deadline = request.form['deadline']
+        done = request.form.get('done') != None
+        new_item = Todo(None, name, deadline, done)
+        new_item.save()
+        return redirect(url_for('list'))
 
 
 @app.route("/remove/<todo_id>")
 def remove(todo_id):
     """ Removes todo item with selected id from the database """
-    return "Remove " + todo_id
+    item = Todo.get_by_id(todo_id)
+    item.delete()
+    return redirect(url_for('list'))
 
 
 @app.route("/edit/<todo_id>", methods=['GET', 'POST'])
@@ -33,13 +45,30 @@ def edit(todo_id):
     If the method was GET it should show todo item form.
     If the method was POST it shold update todo item in database.
     """
-    return "Edit " + todo_id
+    if request.method == 'GET':
 
+        item = Todo.get_by_id(todo_id)
+        return render_template('edit.html', item=item)
+
+    elif request.method == 'POST':
+
+        task_name = request.form['name']
+        deadline = request.form['deadline']
+        done = request.form.get('done') != None
+        item = Todo.get_by_id(todo_id)
+        item.name = task_name
+        item.deadline = deadline
+        item.done = done
+        item.save()
+        return redirect(url_for('list'))
 
 @app.route("/toggle/<todo_id>")
 def toggle(todo_id):
     """ Toggles the state of todo item """
-    return "Toggle " + todo_id
+    item = Todo.get_by_id(todo_id)
+    item.toggle()
+    return redirect(url_for('list'))
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- models/todo.py
+++ models/todo.py
@@ -1,23 +1,48 @@
 import sqlite3
+import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "data/todo.db")
 
 class Todo:
     """ Class representing todo item."""
 
-    def __init__(self, id, name, done=False):
-        pass
+    def __init__(self, id, name, deadline, done=False):
+        self.id = id
+        self.name = name
+        self.deadline = deadline
+        self.done = done
 
     def toggle(self):
         """ Toggles item's state """
-        pass
+        self.done = not self.done
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+        c.execute("UPDATE TodoList SET done = ? WHERE id = ?",
+                  (self.done, self.id,))
+        conn.commit()
+        conn.close()
 
     def save(self):
         """ Saves/updates todo item in database """
-        pass
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+        c.execute("INSERT OR IGNORE INTO TodoList (id, name, deadline, done) VALUES (?, ?, ?, ?)",
+                (self.id, self.name, self.deadline, self.done,))
+        c.execute("UPDATE TodoList SET name = ?, done = ?, deadline = ? WHERE id = ?",
+                                 (self.name, self.done, self.deadline, self.id))
+        conn.commit()
+        conn.close()
+
 
     def delete(self):
         """ Removes todo item from the database """
-        pass
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+        c.execute("DELETE FROM TodoList WHERE id = ?", (self.id,))
+        conn.commit()
+        c.close()
+        return None
 
     @classmethod
     def get_all(cls):
@@ -25,7 +50,17 @@ class Todo:
         Returns:
             list(Todo): list of all todos
         """
-        pass
+        all_todo =[]
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+        c.execute("SELECT * FROM TodoList")
+
+        for row in c:
+            todo = Todo(row[0],row[1],row[2],row[3])
+            all_todo.append(todo)
+
+        conn.close()
+        return all_todo
 
     @classmethod
     def get_by_id(cls, id):
@@ -35,4 +70,13 @@ class Todo:
         Returns:
             Todo: Todo object with a given id
         """
-        pass
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+        c.execute("SELECT * FROM TodoList WHERE id = ?", (id,))
+
+        for row in c:
+            todo = Todo(row[0], row[1], row[2], row[3])
+
+        conn.close()
+        return todo
+
